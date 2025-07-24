9830a0a57cd5526241aaedd8e6eff74ed4b36198
Andrew Guo <andrew.guo5@gmail.com>
2025-07-24 17:52:56 -0600

    Added for base map, but need to more thoroughly test the line scene for different angles

--- BasicTD/Scenes/BasicMapScene.cs
+++ BasicTD/Scenes/BasicMapScene.cs
@@ -7,6 +7,7 @@ using MonoGameLibrary.Graphics;
 using MonoGameLibrary.Input;
 using MonoGameLibrary.Scenes;
 using MonoGameLibrary;
+using MonoGameLibrary.Collision;
 using System.Collections.Generic;
 using MonoGameLibrary.Paths;
 using System;
@@ -33,6 +34,7 @@ public class BasicMapScene : BaseScene
 
     // Towers
     private List<Vector2> Towers;
+    private bool TowerPlacementValid;
     
     public BasicMapScene() : base()
     {
@@ -106,11 +108,19 @@ public class BasicMapScene : BaseScene
             TorchCreep.Update(gameTime);
         }
         
-        if (PlacingTower && Core.Input.Mouse.WasButtonJustPressed(MouseButton.Left))
+        if (PlacingTower) 
         {
             Vector2 mousePos = Core.Input.Mouse.Position.ToVector2();
-            Towers.Add(mousePos);
-            PlacingTower = false;
+            Hitbox TowerBox = new Hitbox(
+                mousePos, (int)(Tower.Width * 0.5f)
+            );
+            TowerPlacementValid = !Path.HasCollided(TowerBox);
+            
+            if (Core.Input.Mouse.WasButtonJustPressed(MouseButton.Left) && TowerPlacementValid)
+            {
+                Towers.Add(mousePos);
+                PlacingTower = false;
+            }
         }
     }
 
@@ -147,7 +157,15 @@ public class BasicMapScene : BaseScene
             Vector2 mousePos = Core.Input.Mouse.Position.ToVector2();
 
             Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
-            Tower.Draw(Core.SpriteBatch, mousePos, new Color(0, 255, 0, 128), 0f);
+
+            Color SemiTransparentGreen = new Color(0, 255, 0, 128);
+            Color SemiTransparentRed = new Color(255, 0, 0, 128);
+
+            if (TowerPlacementValid)
+                Tower.Draw(Core.SpriteBatch, mousePos, SemiTransparentGreen, 0f);
+            else
+                Tower.Draw(Core.SpriteBatch, mousePos, SemiTransparentRed, 0f);
+            
             Core.SpriteBatch.End();
             DrawCircleIndicator();
         }
